108b96ec32924f5c66a04076cb02c931be138451
fiction2st <fiction2st@naver.com>
2025-05-15 11:19:05 +0900

    first commit

---
+++ src/main/java/com/onus/demo_crud/entities/Employee.java
@@ -0,0 +1,32 @@
+package com.onus.demo_crud.entities;
+
+import com.onus.demo_crud.common.AbstractEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@SuperBuilder
+@Table(name = "employees")
+public class Employee extends AbstractEntity {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "department")
+    private String department;
+}
